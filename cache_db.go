049fd60a43167c5f66322293b48899db3fdc6ddc
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-01-01 18:01:37 -0600

    implemented initial functions to save and load messages

--- cache_db.go
+++ cache_db.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	redis "gopkg.in/redis.v3"
 )
 
@@ -14,4 +16,18 @@ func NewRedisClient() *RedisClient {
 		Password: "",
 		DB:       0,
 	}
+}
+
+func (c *RedisClient) SaveMessage(channel, msg string) error {
+	// Key should be in the format of channelName:messages
+	key := fmt.Sprintf("%s:messages", channel)
+
+	return c.client.LPush(key, msg).Err()
+}
+
+func (c *RedisClient) GetAllMessages(channel string) ([]string, error) {
+	// Key should be in the format of channelName:messages
+	key := fmt.Sprintf("%s:messages", channel)
+
+	return c.client.LRange(key, 0, -1)
 }
