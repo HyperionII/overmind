b65a29301fac391b2d538cea44373cc0d12dd240
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-01-03 00:23:16 -0600

    created route for the frontend

--- main.go
+++ main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 )
+
+var homeTemplate = template.Must(template.ParseFiles("static/index.html"))
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	homeTemplate.Execute(w, nil)
+}
 
 func main() {
 	log.Println("Starting server at port 2222...")
 
 	s := NewServer()
 
+	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/echo", s.OnConnect)
 
 	log.Println("Listening...")
