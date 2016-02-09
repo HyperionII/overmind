76ff39836ce1c0f5d9d5b4878055b6265f9fae9f
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-02-09 15:06:28 -0600

    added missing comments on cache_db.go

--- cache_db.go
+++ cache_db.go
@@ -6,10 +6,13 @@ import (
 	redis "gopkg.in/redis.v3"
 )
 
+// RedisClient is a wrapper for the Redis lib to encapsulate common methods
+// used in the application.
 type RedisClient struct {
 	client *redis.Client
 }
 
+// NewRedisClient creates a new RedisClient.
 func NewRedisClient() *RedisClient {
 	return &RedisClient{
 		client: redis.NewClient(&redis.Options{
@@ -20,6 +23,7 @@ func NewRedisClient() *RedisClient {
 	}
 }
 
+// SaveMessage stores a message string.
 func (c *RedisClient) SaveMessage(channel, msg string) error {
 	// Key should be in the format of channelName:messages
 	key := fmt.Sprintf("%s:messages", channel)
@@ -27,6 +31,7 @@ func (c *RedisClient) SaveMessage(channel, msg string) error {
 	return c.client.LPush(key, msg).Err()
 }
 
+// GetAllMessages retreives all messages for the specified channel.
 func (c *RedisClient) GetAllMessages(channel string) ([]string, error) {
 	// Key should be in the format of channelName:messages
 	key := fmt.Sprintf("%s:messages", channel)
@@ -34,6 +39,7 @@ func (c *RedisClient) GetAllMessages(channel string) ([]string, error) {
 	return c.client.LRange(key, 0, -1).Result()
 }
 
+// Ping is used to check if the Redis connection is active.
 func (c *RedisClient) Ping() error {
 	return c.client.Ping().Err()
 }
