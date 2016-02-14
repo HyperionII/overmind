0cc249c372add0e23a83809ad6eb5cd51c95aa9d
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-02-14 15:24:11 -0600

    changed var to constant

--- client.go
+++ client.go
@@ -20,7 +20,7 @@ const (
 	pongWait = 60 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
+	pingPeriod = 54 * time.Second
 
 	// Maximum message size allowed from peer.
 	maxMessageSize = 512
